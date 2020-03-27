e6a462419b3bd0c9ea1a2d7b8e0a49dec2004f4b
andrews-tech <45342431+andrewstech@users.noreply.github.com>
2020-03-27 21:35:26 +0000

    Create hassio.sh

---
+++ hassio.sh
@@ -0,0 +1,8 @@
+cd
+sudo apt-get update
+apt-get install software-properties-common
+y
+apt-get install -y apparmor-utils apt-transport-https avahi-daemon ca-certificates curl dbus jq network-manager socat
+y
+curl -fsSL get.docker.com | sh
+curl -sL "https://raw.githubusercontent.com/home-assistant/hassio-installer/master/hassio_install.sh" | bash -s -- -m raspberrypi3
