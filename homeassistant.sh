500b0bc2e49a172f8f6f6e6085b6953a11e0bed5
andrewstech <45342431+andrewstech@users.noreply.github.com>
2021-03-08 19:45:25 +0000

    Update homeassistant.sh

--- homeassistant.sh
+++ homeassistant.sh
@@ -1,8 +1,10 @@
 
 cd
 sudo apt-get update
-apt-get install software-properties-common
-y
-apt-get install -y apparmor-utils apt-transport-https avahi-daemon ca-certificates curl dbus jq network-manager socat
-y
-curl -sL "https://raw.githubusercontent.com/home-assistant/hassio-installer/master/hassio_install.sh" | bash -s -- -m raspberrypi3
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
+sudo usermod -aG docker root
+sudo usermod -aG docker dietpi
+apt-get install software-properties-common -y
+apt-get install -y apparmor-utils apt-transport-https avahi-daemon ca-certificates curl dbus jq network-manager socat -y
+curl -sL "https://raw.githubusercontent.com/home-assistant/hassio-installer/master/hassio_install.sh" | bash -s -- -m raspberrypi4-64
