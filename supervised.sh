6f55aa2b62678ce6857731f1c13b143f1c2c81ad
andrewstech <45342431+andrewstech@users.noreply.github.com>
2021-03-08 19:50:52 +0000

    Create supervised.sh

---
+++ supervised.sh
@@ -0,0 +1,10 @@
+sudo su
+cd
+sudo apt-get update
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
+sudo usermod -aG docker root
+sudo usermod -aG docker dietpi
+apt-get install software-properties-common -y
+apt-get install -y apparmor-utils apt-transport-https avahi-daemon ca-certificates curl dbus jq network-manager socat -y
+curl -sL "https://raw.githubusercontent.com/home-assistant/hassio-installer/master/hassio_install.sh" | bash -s -- -m raspberrypi4-64
